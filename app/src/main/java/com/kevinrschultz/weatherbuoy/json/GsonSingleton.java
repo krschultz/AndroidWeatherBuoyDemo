344b69554601a581b6929f674ac4095845f7031e
Kevin Schultz <krschultz@gmail.com>
2014-08-31 12:29:03 -0400

    add GSON library & shared singleton

---
+++ app/src/main/java/com/kevinrschultz/weatherbuoy/json/GsonSingleton.java
@@ -0,0 +1,19 @@
+package com.kevinrschultz.weatherbuoy.json;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+/**
+ * @author Kevin Schultz
+ */
+public final class GsonSingleton {
+
+    private static final Gson GSON = new GsonBuilder()
+            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+            .create();
+
+    public static Gson get() {
+        return GSON;
+    }
+}
