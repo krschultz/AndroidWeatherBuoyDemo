6b0609a19b528a8c73bfa4411f711c9aecc5afba
Kevin Schultz <krschultz@gmail.com>
2014-08-31 20:37:39 -0400

    add unit converters

---
+++ app/src/androidTest/java/com/kevinrschultz/weatherbuoy/test/util/UnitConverterTest.java
@@ -0,0 +1,65 @@
+package com.kevinrschultz.weatherbuoy.test.util;
+
+import com.kevinrschultz.weatherbuoy.util.UnitConverter;
+
+import junit.framework.TestCase;
+
+import org.assertj.core.data.Offset;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Kevin Schultz
+ */
+public class UnitConverterTest extends TestCase {
+
+    private static final Offset<Double> PRECISION = Offset.offset(0.01);
+
+    public void testKnotsToKph() {
+        assertThat(UnitConverter.knotsToKph(0.00)).isEqualTo(0.00);
+        assertThat(UnitConverter.knotsToKph(1.00)).isCloseTo(1.85, PRECISION);
+        assertThat(UnitConverter.knotsToKph(5.00)).isCloseTo(9.26, PRECISION);
+        assertThat(UnitConverter.knotsToKph(10.00)).isCloseTo(18.52, PRECISION);
+        assertThat(UnitConverter.knotsToKph(15.00)).isCloseTo(27.78, PRECISION);
+        assertThat(UnitConverter.knotsToKph(20.00)).isCloseTo(37.04, PRECISION);
+    }
+
+    public void testMphToKph() {
+        assertThat(UnitConverter.mphToKph(0.00)).isEqualTo(0.00);
+        assertThat(UnitConverter.mphToKph(1.00)).isCloseTo(1.61, PRECISION);
+        assertThat(UnitConverter.mphToKph(5.00)).isCloseTo(8.05, PRECISION);
+        assertThat(UnitConverter.mphToKph(10.00)).isCloseTo(16.10, PRECISION);
+        assertThat(UnitConverter.mphToKph(15.00)).isCloseTo(24.14, PRECISION);
+        assertThat(UnitConverter.mphToKph(20.00)).isCloseTo(32.19, PRECISION);
+    }
+
+    public void testKphToKnots() {
+        assertThat(UnitConverter.kphToKnots(0.00)).isEqualTo(0.00);
+        assertThat(UnitConverter.kphToKnots(1.00)).isCloseTo(0.54, PRECISION);
+        assertThat(UnitConverter.kphToKnots(10.00)).isCloseTo(5.40, PRECISION);
+        assertThat(UnitConverter.kphToKnots(20.00)).isCloseTo(10.80, PRECISION);
+        assertThat(UnitConverter.kphToKnots(40.00)).isCloseTo(21.60, PRECISION);
+    }
+
+    public void testKphToMph() {
+        assertThat(UnitConverter.kphToMph(0.00)).isEqualTo(0.00);
+        assertThat(UnitConverter.kphToMph(1.00)).isCloseTo(0.62, PRECISION);
+        assertThat(UnitConverter.kphToMph(10.00)).isCloseTo(6.21, PRECISION);
+        assertThat(UnitConverter.kphToMph(20.00)).isCloseTo(12.43, PRECISION);
+        assertThat(UnitConverter.kphToMph(40.00)).isCloseTo(24.85, PRECISION);
+    }
+
+    public void testFeetToMeters() {
+        assertThat(UnitConverter.feetToMeters(0.00)).isEqualTo(0.00);
+        assertThat(UnitConverter.feetToMeters(1.00)).isCloseTo(0.30, PRECISION);
+        assertThat(UnitConverter.feetToMeters(5.00)).isCloseTo(1.52, PRECISION);
+        assertThat(UnitConverter.feetToMeters(10.00)).isCloseTo(3.05, PRECISION);
+    }
+
+    public void testMetersToFeet() {
+        assertThat(UnitConverter.metersToFeet(0.00)).isEqualTo(0.00);
+        assertThat(UnitConverter.metersToFeet(1.00)).isCloseTo(3.28, PRECISION);
+        assertThat(UnitConverter.metersToFeet(5.00)).isCloseTo(16.40, PRECISION);
+        assertThat(UnitConverter.metersToFeet(10.00)).isCloseTo(32.80, PRECISION);
+    }
+}
---
+++ app/src/main/java/com/kevinrschultz/weatherbuoy/util/UnitConverter.java
@@ -0,0 +1,31 @@
+package com.kevinrschultz.weatherbuoy.util;
+
+/**
+ * @author Kevin Schultz
+ */
+public class UnitConverter {
+
+    public static double knotsToKph(double knots) {
+        return knots * 1.8520;
+    }
+
+    public static double mphToKph(double mph) {
+        return mph * 1.60934;
+    }
+
+    public static double kphToKnots(double kph) {
+        return kph * 0.539957;
+    }
+
+    public static double kphToMph(double kph) {
+        return kph * 0.621371;
+    }
+
+    public static double feetToMeters(double feet) {
+        return feet * 0.3048;
+    }
+
+    public static double metersToFeet(double meters) {
+        return meters * 3.28;
+    }
+}
