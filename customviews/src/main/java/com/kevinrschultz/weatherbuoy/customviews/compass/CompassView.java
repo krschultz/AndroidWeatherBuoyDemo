f7bf0fc35fdb58d7bd3ab2d412f39dc4ceec31d5
Kevin Schultz <krschultz@gmail.com>
2014-09-27 22:06:15 -0400

    add seek bars to compass sandbox

--- customviews/src/main/java/com/kevinrschultz/weatherbuoy/customviews/compass/CompassView.java
+++ customviews/src/main/java/com/kevinrschultz/weatherbuoy/customviews/compass/CompassView.java
@@ -1,7 +1,24 @@
 package com.kevinrschultz.weatherbuoy.customviews.compass;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
 /**
  * @author Kevin Schultz (kschultz@gilt.com)
  */
-public class CompassView {
+public class CompassView extends View {
+
+    public CompassView(Context context) {
+        super(context);
+    }
+
+    public CompassView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CompassView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
 }
--- sandbox/src/main/java/com/kevinrschultz/weatherbuoy/sandbox/CompassViewActivity.java
+++ sandbox/src/main/java/com/kevinrschultz/weatherbuoy/sandbox/CompassViewActivity.java
@@ -2,18 +2,77 @@ package com.kevinrschultz.weatherbuoy.sandbox;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
-import com.kevinrschultz.weatherbuoy.customviews.compass.Compass;
+import com.kevinrschultz.weatherbuoy.customviews.compass.CompassView;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 
 public class CompassViewActivity extends Activity {
 
-    private Compass compass;
+    @InjectView(R.id.compass_compass)
+    CompassView compass;
+
+    @InjectView(R.id.compass_wave_seekbar)
+    SeekBar waveSeekBar;
+
+    @InjectView(R.id.compass_wind_seekbar)
+    SeekBar windSeekBar;
+
+    @InjectView(R.id.compass_wave_label)
+    TextView waveLabel;
+
+    @InjectView(R.id.compass_wind_label)
+    TextView windLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compass_view);
+        ButterKnife.inject(this);
+
+        waveSeekBar.setOnSeekBarChangeListener(makeWaveSeekBarListener());
+        windSeekBar.setOnSeekBarChangeListener(makeWindSeekBarListener());
+
+    }
+
+    private SeekBar.OnSeekBarChangeListener makeWaveSeekBarListener() {
+        return new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                double direction = getDirectionFromProgress(progress);
+                waveLabel.setText(String.format("%.1f", direction));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        };
+    }
+
+    private SeekBar.OnSeekBarChangeListener makeWindSeekBarListener() {
+        return new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                double direction = getDirectionFromProgress(progress);
+                windLabel.setText(String.format("%.1f", direction));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        };
+    }
+
+    private double getDirectionFromProgress(int progress) {
+        return (360.0 * (double) progress) / 100.0;
     }
 
 }
